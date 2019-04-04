4592dffd3e867fd67e01d0a67f677cb526c1dfad
wuchuanwei <m18391635536@163.com>
2019-04-04 13:56:55 +0800

    修改testGit

--- GitTest/src/com/kaiya/test/TestGit.java
+++ GitTest/src/com/kaiya/test/TestGit.java
@@ -3,8 +3,8 @@ package com.kaiya.test;
 public class TestGit {
 	public static void main(String[] args) {
 		int a = 10;
-		int b = 20;
-		int sum = a + b;
+		int bbbbbbbbbbb = 20;
+		int sum = a + bbbbbbbbbbb;
 		System.out.println(sum);
 	}
 }
